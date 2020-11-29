52769a2410843f9d73320fab20ef8a78d38d65c8
mdmlb <mlopez0195@gmail.com>
2020-11-29 17:27:12 -0500

    style ok

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+const btnStart = document.querySelector('.main__button');
+
+btnStart.addEventListener("click", function () {
+    window.location.href = './levels.html';
+});
---
+++ scripts/reading.js
@@ -0,0 +1,5 @@
+const btnStart = document.querySelector('.reading__button');
+
+btnStart.addEventListener("click", function () {
+    window.location.href = './criticaltest.html';
+});
